a3183a9538842ebe0ddf39461bcc6c4d8a87bbee
gabrielestefono <gabrielestefono@hotmail.com>
2023-06-27 21:02:41 -0300

    tipos de dados

---
+++ tipos_primitivos/index.php
@@ -0,0 +1,15 @@
+<?php
+
+/* 
+
+String = "1512"
+Inteiro = 5151
+Float  = 10815.65161
+Lógico = 1 / 0 - False / True;
+
+*/
+
+$nomeCompleto = "Gabriel Estéfono Gomes de Sousa";
+$idade = 27;
+$peso = 66.5;
+$homem = true;
