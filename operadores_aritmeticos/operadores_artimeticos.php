1250cd7fbde0a9330027a1189e71169946d655bf
gabrielestefono <gabrielestefono@hotmail.com>
2023-06-30 12:35:08 -0300

    Operadores Aritméticos

---
+++ operadores_aritmeticos/operadores_artimeticos.php
@@ -0,0 +1,19 @@
+<?php
+
+$num1 = 10;
+$num2 = 2;
+$soma = $num1 + $num2;
+$subtracao = $num1 - $num2;
+$divisao =$num1 / $num2;
+$multiplicacao = $num1 * $num2;
+
+echo $soma . PHP_EOL;
+echo $subtracao . PHP_EOL;
+echo $divisao . PHP_EOL;
+echo $multiplicacao . PHP_EOL;
+
+$modulo = $num1 % $num2;
+$potencia = $num2 ** $num1;
+
+echo $modulo . PHP_EOL;
+echo $potencia . PHP_EOL;
