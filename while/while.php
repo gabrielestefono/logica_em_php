fed6b4e5a9a6c39b04dd72f5b5f70d6befa2422d
gabrielestefono <gabrielestefono@hotmail.com>
2023-07-02 15:28:36 -0300

    While

---
+++ while/while.php
@@ -0,0 +1,14 @@
+<?php
+
+$contador = 0;
+/*
+while($contador < 5){
+    echo $contador . PHP_EOL;
+    $contador++;
+}*/
+
+do{
+    $contador + 1 . PHP_EOL;
+    $contador++;
+    echo $contador . PHP_EOL;
+}while($contador < 5);
