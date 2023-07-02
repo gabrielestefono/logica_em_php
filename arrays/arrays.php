0ba8fbb8b099fc137d6a9f2fa0b9c7da78a6addd
gabrielestefono <gabrielestefono@hotmail.com>
2023-07-02 15:28:14 -0300

    Arrays

---
+++ arrays/arrays.php
@@ -0,0 +1,14 @@
+<?php
+
+$lista = ["João", 27, "Maranhão", "Solteiro"];
+$lista2 = ["Abel", 35, "São Paulo", "Casado"];
+$lista3 = [];
+
+$arrayAssoc = [
+    "nome" => "Gabriel",
+    "idade" => 27,
+    "estado_civil" => "Casado",
+    "estado" => "Paraná"
+];
+
+print_r($arrayAssoc);
