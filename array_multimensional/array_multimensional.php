18b81843c4745fc845a14305d89dc6f2249dcb69
gabrielestefono <gabrielestefono@hotmail.com>
2023-07-02 15:28:27 -0300

    Arrays multidimensionais

---
+++ array_multimensional/array_multimensional.php
@@ -0,0 +1,11 @@
+<?php
+
+$arrayMulti = [
+    ["Escola A", "Escola B", "Escola C", "Escola D"],
+    ["CNPJ A","CNPJ B","CNPJ C","CNPJ D"],
+    ["Pernambuco", "Alagoas", "Rio de janeiro", "São Paulo"]
+];
+
+print_r($arrayMulti);
+print_r($arrayMulti[0]);
+print_r($arrayMulti[0][0]);
